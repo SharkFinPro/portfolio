9a6ddae360480e6665dad611e965150c261f2a84
Alex Martin <sharkfinpro@hotmail.com>
2023-06-12 18:39:42 -0700

    Hello, World!
     - Installed next, react, react-dom, typescript

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Portfolio",
+  description: "Hello, world!"
+};
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Hello, World</h1>
+    </div>
+  );
+}
