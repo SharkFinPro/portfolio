b32fcf691115badd002a1c150aa0f0b932b5d7d6
Alex Martin <sharkfinpro@hotmail.com>
2023-06-21 23:33:05 -0700

    default title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Metadata } from "next";
 import "@/stylesheets/global.css";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Alex Martin",
+    default: "Alex Martin"
+  }
+}
 
 export default function RootLayout({
   children,
