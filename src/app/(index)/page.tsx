fd3ed45a82a5b024da05c2b3dc70ad6cbed5f1d1
Alex Martin <sharkfinpro@hotmail.com>
2023-11-22 22:55:21 -0800

    basic header

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import indexStyles from "./index.module.css";
 
 export const metadata: Metadata = {
   description: "Hello, world!"
@@ -6,8 +7,12 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <div>
-      <h1>Hello, World</h1>
+    <div className={indexStyles.wrapper}>
+      <div className={indexStyles.header}>
+        <div className={indexStyles.container}>
+          <h1>Alex Martin</h1>
+        </div>
+      </div>
     </div>
   );
 }
